e536f77a62bfac80352f8ffb4cb75196bfa64c06
Dharma <dharmatejapamarthi143@gmail.com>
2026-07-01 12:13:12 +0530

    fix mail config

--- backend/config/mail.config.js
+++ backend/config/mail.config.js
@@ -1,13 +1,13 @@
 import nodemailer from "nodemailer";
 
-// console.log(process.env.EMAIL_USER)
-
 const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-    },
+	host: "smtp.gmail.com",
+	port: 587,
+	secure: false, 
+	auth: {
+		user: process.env.EMAIL_USER,
+		pass: process.env.EMAIL_PASS,
+	},
 });
 
 export default transporter;
